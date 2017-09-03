58c651e7e6480d2b74ca4290caa25c5900a1941f
Pavlin Gergov <gergov.pavlin@gmail.com>
2017-09-03 18:35:09 +0300

    Add button that shows/hides goodies

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {Button} from 'reactstrap';
+
+import GoodsComponent from './Goods';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showGoodies: false
+    };
+  }
+
   render() {
+    const {showGoodies} = this.state;
+    const goodsComponent = showGoodies ? <GoodsComponent /> : '';
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Button
+            data-test="show-goodies-button"
+            onClick={() => this.setState({showGoodies: !showGoodies})}>
+            {showGoodies ? 'Hide  goodies' : 'Show goodies'}
+          </Button>
         </p>
+        <div>
+          {goodsComponent}
+        </div>
       </div>
     );
   }
